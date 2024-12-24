642d1000738e695864e8456db4cecbe45f71f089
justkmode <136920389+justkmode@users.noreply.github.com>
2024-12-24 16:16:35 +0100

    Added second EC2 instance in AZ2

--- project/lib/ec2-stack.ts
+++ project/lib/ec2-stack.ts
@@ -7,3 +7,42 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 interface EC2StackProps extends cdk.StackProps {
     vpc: ec2.Vpc;
 }
+
+export class EC2Stack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: EC2StackProps) {
+        super(scope, id, props);
+
+        // EC2 Instance
+
+        const instance1 = new ec2.Instance(this, "MyPrivateEC2-AZ1", {
+            vpc: props.vpc,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                availabilityZones: [props.vpc.availabilityZones[0]]
+            },
+            machineImage: new ec2.AmazonLinuxImage({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+        })    
+
+        cdk.Tags.of(instance1).add("Name", "MyPrivateEC2-AZ1" )
+   
+        // EC2 Instance 2 in AZ 2
+
+        const instance2 = new ec2.Instance(this, "MyPrivateEC2-AZ2", {
+            vpc: props.vpc, 
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                availabilityZones: [props.vpc.availabilityZones[1]]
+            },
+            machineImage: new ec2.AmazonLinuxImage({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+
+        })
+        cdk.Tags.of(instance2).add("Name", "MyPrivateEC2-AZ2" )
+    }
+    
+}
